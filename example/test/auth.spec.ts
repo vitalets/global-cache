2f373a4bae4d48a2fbdb245a9ac47f4bb0f3505f
vitalets <noginsk@rambler.ru>
2025-07-28 14:30:55 +0300

    update example

--- example/test/auth.spec.ts
+++ example/test/auth.spec.ts
@@ -1,31 +1,14 @@
-import { test, expect } from '@playwright/test';
-import { globalStorage } from '@vitalets/global-storage';
-
-test.use({
-  storageState: async ({ browser }, use) => {
-    // authenticate once, then re-use the storage state in all tests in this file.
-    const storageState = await globalStorage.get('storage-state', { ttl: '1 min' }, async () => {
-      const loginPage = await browser.newPage();
-      await loginPage.goto('https://authenticationtest.com/simpleFormAuth/');
-      await loginPage.getByLabel('E-Mail Address').fill('simpleForm@authenticationtest.com');
-      await loginPage.getByLabel('Password').fill('pa$$w0rd');
-      await loginPage.getByRole('button', { name: 'Log In' }).click();
-      await expect(loginPage.getByRole('heading', { name: 'Login Success' })).toBeVisible();
-
-      return loginPage.context().storageState();
-    });
-    await use(storageState);
-  },
-});
+import { expect } from '@playwright/test';
+import { test } from './fixtures';
 
 test('failing test', async ({ page }) => {
   await page.goto('https://authenticationtest.com');
-  await expect(page.getByRole('link', { name: 'Sign Out' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Sign Out' })).toBeVisible(); // authenticated!
   throw new Error('This test is expected to fail');
 });
 
 test('passing test', async ({ page }) => {
   // although previous test fails and new worker is stared, auth state is re-used
   await page.goto('https://authenticationtest.com');
-  await expect(page.getByRole('link', { name: 'Sign Out' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Sign Out' })).toBeVisible(); // authenticated!
 });
---
+++ example/test/fixtures.ts
@@ -0,0 +1,23 @@
+import { test as baseTest, expect } from '@playwright/test';
+import { globalStorage } from '@vitalets/global-storage';
+
+export const test = baseTest.extend({
+  storageState: async ({ storageState, browser }, use, testInfo) => {
+    if (!testInfo.tags.includes('@no-auth')) {
+      // - perform auth only for tests not marked with '@no-auth' tag
+      // - authenticate once, then re-use the storage state in all tests
+      // - re-use auth for subsequent test-runs within 5 minutes
+      storageState = await globalStorage.get('storage-state', { ttl: '5 min' }, async () => {
+        const loginPage = await browser.newPage(); // <-- important to use 'browser', not 'page' or 'context' fixture to avoid cyclic dependency
+        await loginPage.goto('https://authenticationtest.com/simpleFormAuth/');
+        await loginPage.getByLabel('E-Mail Address').fill('simpleForm@authenticationtest.com');
+        await loginPage.getByLabel('Password').fill('pa$$w0rd');
+        await loginPage.getByRole('button', { name: 'Log In' }).click();
+        await expect(loginPage.getByRole('heading', { name: 'Login Success' })).toBeVisible();
+
+        return loginPage.context().storageState();
+      });
+    }
+    await use(storageState);
+  },
+});
