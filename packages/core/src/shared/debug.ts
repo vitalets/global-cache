3e0e7c934524cbe04032d175ca6b21d2e79dddf7
vitalets <noginsk@rambler.ru>
2025-12-05 11:17:33 +0100

    debug: lazy init

--- packages/core/src/shared/debug.ts
+++ packages/core/src/shared/debug.ts
@@ -1,7 +1,24 @@
+/**
+ * Debugging.
+ * Use lazy debug initialization to pick env variables from runtime (e.g. Playwright config).
+ */
 import Debug from 'debug';
 
-export const debug = Debug('global-cache');
+const cache = new Map<string, Debug.Debugger>();
+
+export function debug(...args: Parameters<Debug.Debugger>) {
+  getDebug('global-cache')(...args);
+}
+
 export const debugKey = (key: string, message: string) => {
-  const debug = Debug(`global-cache:${key}`);
-  debug(message);
+  getDebug(`global-cache:${key}`)(message);
 };
+
+function getDebug(key: string) {
+  let debug = cache.get(key);
+  if (!debug) {
+    debug = Debug(key);
+    cache.set(key, debug);
+  }
+  return debug;
+}
